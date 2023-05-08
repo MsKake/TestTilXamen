26103121790c353eb8aa7ec20b076f78af83a811
Scott Allistair Bargamento Eriksen <scotter@viken.no>
2023-05-08 10:44:16 +0200

    Add project files.

---
+++ TestTilXamen/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestTilXamen.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Xamen</title>
+
+</head>
+<body onload="setTemperatureColor()">
+    
+    <form id="form1" runat="server">
+        <div>
+            <p>Temperatur:</p>
+            <asp:Label ID="LabelAirTemp" runat="server" Text="Label"></asp:Label>
+        </div>
+        <br />
+
+        <div>
+            <p>Vindstyrke:</p>
+            <asp:Label ID="LabelAirSpeed" runat="server" Text="Label"></asp:Label>
+        </div>
+        <br />
+
+        <div>
+            <p>Vindretning:</p>
+            <asp:Label ID="LabelWindDirection" runat="server" Text="Label"></asp:Label>
+        </div>
+        <br />
+
+        <div>
+            <p>Oppgave 14:</p>
+            <asp:Label ID="resultatLabel" runat="server" Text="Label"></asp:Label>
+        </div>
+        <br />
+
+
+
+    </form>
+</body>
+</html>
---
+++ TestTilXamen/Default.aspx.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace TestTilXamen
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Oppgave1();
+            Oppgave2();
+            Oppgave3();
+            string startString = "Spiser boller og brus fra rema.";
+            string omvendtString = Oppgave14(startString);
+            resultatLabel.Text = omvendtString;
+        }
+       
+        public int Oppgave1()
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+
+            //the usual stuff. run the request, parse your json
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+
+                    double temp = data.Value<double>("air_temperature");
+                    LabelAirTemp.Text=temp.ToString();
+
+
+
+
+                    //key name - getting key.value
+                    //int valuepm25 = data.Value<int>("pm25");
+                    //int radonValue = data.Value<int>("radonShortTermAvg");
+                    // inn i db
+
+                }
+                return 0;
+            }
+            catch { Exception ex; }
+            return 0;
+        }
+
+
+
+        public int Oppgave2()
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+
+            //the usual stuff. run the request, parse your json
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+
+                    double speed = data.Value<double>("wind_speed");
+                    LabelAirSpeed.Text = speed.ToString();
+
+
+
+
+                    //key name - getting key.value
+                    //int valuepm25 = data.Value<int>("pm25");
+                    //int radonValue = data.Value<int>("radonShortTermAvg");
+                    // inn i db
+
+                }
+                return 0;
+            }
+            catch { Exception ex; }
+            return 0;
+        }
+
+
+
+        public int Oppgave3()
+        {
+            //http://jsonviewer.stack.hu/
+            //https://peterdaugaardrasmussen.com/2022/01/18/how-to-get-a-property-from-json-using-selecttoken/
+            //create the httpwebrequest
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166");
+
+            //the usual stuff. run the request, parse your json
+            try
+            {
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "GET";
+                httpWebRequest.UserAgent = "bolle";
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    JObject jObj = JObject.Parse(result);
+                    JToken data = jObj.SelectToken("properties.timeseries[0].data.instant.details");
+
+
+                    double direction = data.Value<double>("wind_from_direction");
+                    LabelWindDirection.Text = direction.ToString();
+
+
+
+
+                    //key name - getting key.value
+                    //int valuepm25 = data.Value<int>("pm25");
+                    //int radonValue = data.Value<int>("radonShortTermAvg");
+                    // inn i db
+
+                }
+                return 0;
+            }
+            catch { Exception ex; }
+            return 0;
+        }
+
+
+
+        public static string Oppgave14(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+    }
+}
